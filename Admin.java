3072d7b1d5658c26b433cf5bf656cf4f705f87ab
tedolight <tedit3833@gmail.com>
2025-05-14 14:48:37 +0300

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,40 @@
+public class Admin {
+
+	private int adminId;
+	private String role;
+	private String adminName;
+	private int age;
+	private String username;
+	private String password;
+
+	public void assignRole() {
+		// TODO - implement Admin.assignRole
+		throw new UnsupportedOperationException();
+	}
+
+	public void viewReport() {
+		// TODO - implement Admin.viewReport
+		throw new UnsupportedOperationException();
+	}
+
+	public void manageRestaurantData() {
+		// TODO - implement Admin.manageRestaurantData
+		throw new UnsupportedOperationException();
+	}
+
+	public void manageUser() {
+		// TODO - implement Admin.manageUser
+		throw new UnsupportedOperationException();
+	}
+
+	public void login() {
+		// TODO - implement Admin.login
+		throw new UnsupportedOperationException();
+	}
+
+	public void logout() {
+		// TODO - implement Admin.logout
+		throw new UnsupportedOperationException();
+	}
+
+}
